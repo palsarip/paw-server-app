c0d7926bc8cb4b47705f180ccf05c3b640d0bdc6
Glitch (whatsapp-cloud-api-echo-bot) <none>
2024-04-23 04:08:31 +0000

    🐪🚪 Checkpoint ./server.js:66788403/23403

--- server.js
+++ server.js
@@ -11,18 +11,26 @@ import axios from "axios";
 const app = express();
 app.use(express.json());
 
-const { WEBHOOK_VERIFY_TOKEN, GRAPH_API_TOKEN, PORT, CLOUD_API_VERSION, OPENAI_API_KEY } = process.env;
+const {
+  WEBHOOK_VERIFY_TOKEN,
+  GRAPH_API_TOKEN,
+  PORT,
+  CLOUD_API_VERSION,
+  OPENAI_API_KEY,
+} = process.env;
+
+/* Variables */
+let chatWithPAW = false;
 
 app.post("/webhook", async (req, res) => {
   try {
-    /* Variables */
-    let chatWithPAW = false;
-
     console.log("Incoming webhook message:", JSON.stringify(req.body, null, 2));
 
     const message = req.body.entry?.[0]?.changes[0]?.value?.messages?.[0];
 
-    switch (message.type === "text" && chatWithPAW === false) {
+    switch (chatWithPAW) {
+        case (chatWithPAW === true): 
+        console.log("chatWithPAW", chatWithPAW)
       default:
         const business_phone_number_id =
           req.body.entry?.[0].changes?.[0].value?.metadata?.phone_number_id;
@@ -36,7 +44,7 @@ app.post("/webhook", async (req, res) => {
           data: {
             messaging_product: "whatsapp",
             recipient_type: "individual",
-            to: message.from  ?? "WhatsApp User",
+            to: message.from ?? "WhatsApp User",
             type: "interactive",
             interactive: {
               type: "button",
@@ -88,6 +96,8 @@ app.post("/webhook", async (req, res) => {
           },
         });
         break;
+      case chatWithPAW === true:
+        console.log("chatWithPAW", chatWithPAW);
     }
 
     res.sendStatus(200);
